cd1b08b7cb436cbd7f78ae406e412b6ac72c3f6b
Jacob <jsotunde@ufl.edu>
2026-07-07 11:45:21 -0400

    converted db into flask

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///eventdata.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+class Weather(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    temperature = db.Column(db.Float, nullable=False)
+    humidity = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(200), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    venue = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    latitude = db.Column(db.Float, nullable=True)
+    longitude = db.Column(db.Float, nullable=True)
+    forecast = db.relationship('Weather', backref='event', lazy=True)
+    weather_id = db.Column(db.Integer, db.ForeignKey('weather.id'), nullable=True)
+def init_database():
+    db.create_all()
+if __name__ == "__main__":
+    init_database()
+    app.run(debug=True)
